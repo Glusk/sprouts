79986151575108296ed1f9ff784628d322b243c0
Glusk2 <lj.slo@hotmail.com>
2018-06-24 12:17:56 +0200

    #2 Replaced matrix inversion with Thomas algorithm.

--- core/src/main/java/com/github/glusk2/sprouts/geom/BSplineControlPoints.java
+++ core/src/main/java/com/github/glusk2/sprouts/geom/BSplineControlPoints.java
@@ -1,8 +1,9 @@
 package com.github.glusk2.sprouts.geom;
 
 import com.badlogic.gdx.math.Vector2;
-import org.la4j.inversion.GaussJordanInverter;
-import org.la4j.matrix.DenseMatrix;
+import org.la4j.Vector;
+import org.la4j.linear.LinearSystemSolver;
+import org.la4j.linear.SweepSolver;
 import org.la4j.matrix.SparseMatrix;
 
 import java.util.ArrayList;
@@ -32,34 +33,32 @@ public class BSplineControlPoints implements Polyline {
         }
 
         double[][] m = new double[n-1][n-1];
-        double[][] c = new double[n-1][2];
+        double[] cX = new double[n-1];
+        double[] cY = new double[n-1];
 
         System.arraycopy(ONE_FOUR_ONE, 1, m[0], 0, 2);
-        c[0][0] = 6 * s[1].x - s[0].x;
-        c[0][1] = 6 * s[1].y - s[0].y;
+        cX[0] = 6 * s[1].x - s[0].x;
+        cY[0] = 6 * s[1].y - s[0].y;
         for (int i = 1; i < m.length - 1; i++) {
             System.arraycopy(ONE_FOUR_ONE, 0, m[i],i - 1,3);
-            c[i][0] = 6 * s[i+1].x;
-            c[i][1] = 6 * s[i+1].y;
+            cX[i] = 6 * s[i+1].x;
+            cY[i] = 6 * s[i+1].y;
         }
         System.arraycopy(ONE_FOUR_ONE, 0, m[n-2], n-3, 2);
-        c[n-2][0] = 6 * s[n-1].x - s[n].x;
-        c[n-2][1] = 6 * s[n-1].y - s[n].y;
+        cX[n-2] = 6 * s[n-1].x - s[n].x;
+        cY[n-2] = 6 * s[n-1].y - s[n].y;
 
-        double[][] b =
-            new GaussJordanInverter(
-                SparseMatrix.from2DArray(m)
-            ).inverse().multiply(
-                DenseMatrix.from2DArray(c)
-            ).toDenseMatrix().toArray();
+        LinearSystemSolver lss = new SweepSolver(SparseMatrix.from2DArray(m));
+        Vector bX = lss.solve(Vector.fromArray(cX));
+        Vector bY = lss.solve(Vector.fromArray(cY));
 
         List<Vector2> controlPoints = new ArrayList<Vector2>(N);
         controlPoints.add(s[0]);
-        for (double[] point : b) {
+        for (int i = 0; i < n-1; i++) {
             controlPoints.add(
                 new Vector2(
-                    (float) point[0],
-                    (float) point[1]
+                    (float) bX.get(i),
+                    (float) bY.get(i)
                 )
             );
         }
