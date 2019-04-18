81f6ed798e8f26ac209a234b5b91d9791146dd9b
Glusk2 <lj.slo@hotmail.com>
2019-04-18 11:57:56 +0200

    #31 Added a straight line DirectedEdge impl.

---
+++ core/src/main/java/com/github/glusk2/sprouts/comb/StraightLineEdge.java
@@ -0,0 +1,91 @@
+package com.github.glusk2.sprouts.comb;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.Vector2;
+import com.github.glusk2.sprouts.geom.Polyline;
+
+/**
+ * Represents a straight line directed Graph edge between {@code from()} and
+ * {@code to()}.
+ */
+public final class StraightLineEdge implements DirectedEdge {
+    /** Default Edge color. */
+    private static final Color DEFAULT_COLOR = Color.BLACK;
+    /** The color of {@code this} DirectedEdge.*/
+    private final Color color;
+    /** The source Vertex of {@code this} DirectedEdge. */
+    private Vertex from;
+    /** The destination Vertex of {@code this} DirectedEdge. */
+    private Vertex to;
+
+    /**
+     * Constructs a new StraightLineEdge from its endpoints and the default
+     * edge color.
+     *
+     * @param from the source Vertex of the DirectedEdge
+     * @param to the destination Vertex of the DirectedEdge
+     */
+    public StraightLineEdge(final Vertex from, final Vertex to) {
+        this(DEFAULT_COLOR, from, to);
+    }
+
+    /**
+     * Constructs a new StraightLineEdge from its endpoints and the specified
+     * edge color.
+     *
+     * @param color the color of the DirectedEdge
+     * @param from the source Vertex of the DirectedEdge
+     * @param to the destination Vertex of the DirectedEdge
+     */
+    public StraightLineEdge(
+        final Color color,
+        final Vertex from,
+        final Vertex to) {
+        this.color = color;
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public Vertex from() {
+        return from;
+    }
+
+    @Override
+    public Vertex to() {
+        return to;
+    }
+
+    @Override
+    public Color color() {
+        return color;
+    }
+
+    @Override
+    public Polyline polyline() {
+        List<Vector2> endpoints = new ArrayList<Vector2>();
+        endpoints.add(from().position());
+        endpoints.add(to().position());
+        return new Polyline.WrappedList(endpoints);
+    }
+
+    @Override
+    public int hashCode() {
+        return (from().hashCode() + "-" + to().hashCode()).hashCode();
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || !(obj instanceof DirectedEdge)) {
+            return false;
+        }
+        DirectedEdge that = (DirectedEdge) obj;
+        return from().equals(that.from()) && to().equals(that.to());
+    }
+}
