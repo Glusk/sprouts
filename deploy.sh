873278359492f5d69bad736d7646830183fe6aae
Glusk2 <lj.slo@hotmail.com>
2019-09-29 03:20:52 +0200

    #113 Added itchio deployment script

---
+++ deploy.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+if [[ -z "${BUTLER_API_KEY}" ]]; then
+  echo "Unable to deploy! No BUTLER_API_KEY environment variable specified!"
+  exit 1
+fi
+
+prepare_butler() {
+    echo "Preparing butler..."
+    download_if_not_exist http://dl.itch.ovh/butler/linux-amd64/head/butler butler
+    chmod +x butler
+}
+
+prepare_and_push() {
+    echo "Push $3 build to itch.io..."
+    ./butler push $2 $1:$3
+}
+
+download_if_not_exist() {
+    if [ ! -f $2 ]; then
+        curl -L -O $1 > $2
+    fi
+}
+
+project="glusk2/sprouts"
+desktop="desktop/build/libs/sprouts-$TRAVIS_TAG.jar"
+android="android/build/outputs/apk/sprouts-$TRAVIS_TAG-unaligned.apk"
+html="sprouts-$TRAVIS_TAG.zip"
+
+
+prepare_butler
+
+prepare_and_push $project $desktop "windows-linux-mac-stable" --userversion $TRAVIS_TAG
+prepare_and_push $project $android "android" --userversion $TRAVIS_TAG
+prepare_and_push $project $html "html" --userversion $TRAVIS_TAG
+
+echo "Done."
+exit 0
