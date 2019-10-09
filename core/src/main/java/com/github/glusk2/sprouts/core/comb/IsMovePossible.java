937060a85b8099d05aed2d1145c149886432cd5a
Glusk2 <lj.slo@hotmail.com>
2019-10-09 22:49:31 +0200

    #148 Added 'no-more-moves' check for graph

---
+++ core/src/main/java/com/github/glusk2/sprouts/core/comb/IsMovePossible.java
@@ -0,0 +1,33 @@
+package com.github.glusk2.sprouts.core.comb;
+
+import java.util.List;
+import java.util.Set;
+
+import com.github.glusk2.sprouts.core.util.Check;
+
+/** A check that tests whether a Move is possible in {@code gameState}. */
+public final class IsMovePossible implements Check {
+
+    /** The Graph to check for valid Moves. */
+    private final Graph gameState;
+
+    /**
+     * Creates a new Check by specifying the {@code gameState}.
+     *
+     * @param gameState the Graph to check for valid Moves
+     */
+    public IsMovePossible(final Graph gameState) {
+        this.gameState = gameState;
+    }
+
+    @Override
+    public boolean check() {
+        List<Set<CompoundEdge>> faces = gameState.faces();
+        for (Set<CompoundEdge> face : faces) {
+            if (new IsMovePossibleInFace(gameState, face).check()) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
