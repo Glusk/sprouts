0d86613372c8de58cc105d4a1fc02e218002211a
Glusk <lj.slo@hotmail.com>
2021-04-03 19:40:16 +0200

    #205 Rename for consistency to separate olf & new

--- core/src/test/java/com/github/glusk2/sprouts/core/comb/SproutsEdgeTest.java
+++ core/src/test/java/com/github/glusk2/sprouts/core/comb/SproutsEdgeTest.java
@@ -14,7 +14,7 @@ import com.github.glusk2.sprouts.core.geom.Polyline;
 
 import org.junit.Test;
 
-public final class DirectedEdgeTest {
+public final class SproutsEdgeTest {
     @Test
     public void oppositePointersAreNotNotEqual() {
         assertThat(
