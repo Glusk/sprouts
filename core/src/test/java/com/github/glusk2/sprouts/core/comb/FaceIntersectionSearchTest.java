72807db3a9e1b1e2c9fd15e7c5b6c1e9993883ff
Glusk <lj.slo@hotmail.com>
2021-04-07 15:45:09 +0200

    #205 Fix a test to use new API

--- core/src/test/java/com/github/glusk2/sprouts/core/comb/FaceIntersectionSearchTest.java
+++ core/src/test/java/com/github/glusk2/sprouts/core/comb/FaceIntersectionSearchTest.java
@@ -2,11 +2,13 @@ package com.github.glusk2.sprouts.core.comb;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.math.Vector2;
+import com.github.glusk2.sprouts.core.geom.Polyline;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -14,34 +16,41 @@ import org.junit.Test;
 /** FaceIntersectionSearch test class. */
 public final class FaceIntersectionSearchTest {
     /** A simple face, shared by various tests. */
-    private static Set<CompoundEdge> face;
+    private static Set<SproutsEdge> face;
 
     /** Sets up a simple square box face, used for simple tests. */
     @BeforeClass
     public static void setupFace() {
-        face = new HashSet<CompoundEdge>();
-        Vertex v0 = new PresetVertex(Vector2.Zero);
-        Vertex v1 = new PresetVertex(new Vector2(2, 0));
-        Vertex v2 = new PresetVertex(new Vector2(2, 2));
-        Vertex v3 = new PresetVertex(new Vector2(0, 2));
-        face.add(
-            new CompoundEdge.Wrapped(
-                new StraightLineEdge(v0, v1)
-            )
-        );
-        face.add(
-            new CompoundEdge.Wrapped(
-                new StraightLineEdge(v1, v2)
-            )
-        );
-        face.add(
-            new CompoundEdge.Wrapped(
-                new StraightLineEdge(v2, v3)
-            )
-        );
-        face.add(
-            new CompoundEdge.Wrapped(
-                new StraightLineEdge(v3, v0)
+        face = new HashSet<SproutsEdge>(
+            Arrays.asList(
+                new SproutsEdge(
+                    new Polyline.WrappedList(
+                        Vector2.Zero,
+                        new Vector2(2, 0)
+                    ),
+                    Color.BLACK, Color.BLACK
+                ),
+                new SproutsEdge(
+                    new Polyline.WrappedList(
+                        new Vector2(2, 0),
+                        new Vector2(2, 2)
+                    ),
+                    Color.BLACK, Color.BLACK
+                ),
+                new SproutsEdge(
+                    new Polyline.WrappedList(
+                        new Vector2(2, 2),
+                        new Vector2(0, 2)
+                    ),
+                    Color.BLACK, Color.BLACK
+                ),
+                new SproutsEdge(
+                    new Polyline.WrappedList(
+                        new Vector2(0, 2),
+                        Vector2.Zero
+                    ),
+                    Color.BLACK, Color.BLACK
+                )
             )
         );
     }
