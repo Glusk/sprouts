e06903b1dc0d4978f324912bb359a0be41acf43a
Glusk <lj.slo@hotmail.com>
2021-04-17 11:01:52 +0200

    #205 SproutsRotations cleanup

--- core/src/main/java/com/github/glusk2/sprouts/core/comb/SproutsRotations.java
+++ core/src/main/java/com/github/glusk2/sprouts/core/comb/SproutsRotations.java
@@ -5,22 +5,50 @@ import java.util.SortedSet;
 import java.util.TreeSet;
 
 /**
- * Only edges with the same origin (from()) shall be valid as constructor
- * arguments. Otherwise, invocations of {@code next()} will error with
+ * Sprouts local Rotations.
+ * <p>
+ * Represents a local rotations object of edges with a common origin
+ * ({@link SproutsEdge#from()}).
+ * <p>
+ * Only edges with the same origin shall be valid as constructor
+ * arguments. Otherwise, invocations of {@link #next()} will error with
  * {@code IllegalArgumentException}.
  */
 public final class SproutsRotations {
-
+    /** Common origin edges. */
     private SortedSet<SproutsEdge> edges;
 
-    public SproutsRotations(SproutsEdge... edges) {
+    /**
+     * Creates local rotations from an array of {@code edges}.
+     *
+     * @param edges common origin edges
+     */
+    public SproutsRotations(final SproutsEdge... edges) {
         this(new TreeSet<>(Arrays.asList(edges)));
     }
-    public SproutsRotations(SortedSet<SproutsEdge> edges) {
+    /**
+     * Creates local rotations from an sorted set of {@code edges}.
+     *
+     * @param edges common origin edges
+     */
+    public SproutsRotations(final SortedSet<SproutsEdge> edges) {
         this.edges = edges;
     }
 
-    public SproutsEdge next(SproutsEdge current) {
+    /**
+     * Returns the first SproutsEdge after {@code current} in this local
+     * rotations object.
+     * <p>
+     * {@code current} need not be a part of this local rotations object,
+     * but it must have the same origin
+     * ({@link SproutsEdge#from()}).
+     *
+     * @param current an edge in that shares the same origin
+     * ({@link SproutsEdge#from()}) with edges in {@code this} local rotations
+     * @return the first edge after {@code current} in this local rotations
+     *         object
+     */
+    public SproutsEdge next(final SproutsEdge current) {
         SortedSet<SproutsEdge> withCurrent = new TreeSet<>(edges);
         withCurrent.add(current);
 
